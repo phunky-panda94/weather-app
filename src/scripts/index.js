71392c08ec7b8d619a24447896968de85c330666
phunky-panda94 <victor.nim94@outlook.com>
2021-10-12 10:00:06 +1100

    implemented fetchGif

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
-import apiKey from '../../apikey.js';
+import { weatherKey, gifKey } from '../../apikeys.js';
 
 // fetch weather data for current location using Promises 
 const fetchWeather = (location) => {
 
-    let api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${weatherKey}&units=metric`
 
     return fetch(api, { mode: 'cors' })
     .then(response => response.json())
@@ -15,13 +15,12 @@ const fetchWeather = (location) => {
             weather: json.weather[0].main,
             description: json.weather[0].description
         }
-    });
+    })
 
 }
 
 // display weather data
 const details = document.querySelector('ul');
-
 
 const displayData = (data) => {
 
@@ -48,13 +47,38 @@ search.addEventListener('submit', (e) => {
     e.preventDefault();
     let location = search.elements['location'].value;
 
-    fetchWeather(location).then(data => displayData(data));
+    fetchWeather(location).then(data => {
+        displayData(data);
+        console.log(data['weather']);
+        fetchGif(`weather ${data['weather']}`);
+    // TODO: handle error
+    }).catch(err => {
+        let detail = document.createElement('li');
+        detail.textContent = 'Woops! Looks like something went wrong...';
+        details.append(detail);
+        img.style.display = 'none';
+    })
+    
 
     search.reset();
 
 });
 
-// TODO: fetch gif based on weather data using async + await
+// fetch gif based on weather data using async + await
+const img = document.querySelector('img');
 
-// TODO: add loading animation -> https://www.w3schools.com/howto/howto_css_loader.asp
+const fetchGif = async (keyword) => {
+
+    let data = `https://api.giphy.com/v1/gifs/translate?api_key=${gifKey}=${keyword}`;
+
+    const response = await fetch(data, { mode: 'cors' });
+    const json = await response.json();
+    img.style.display = 'block';
+    img.src = json.data.images.original.url;
+
+}
+
+// add loading animation
+// const loader = document.querySelector('.loader');
+// loader.classList.toggle('none');
 
